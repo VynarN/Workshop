4aa5722d1a3e50d14039da0e9a62aa76c556cdce
Назар Винар <vynar.n.i@gmail.com>
2019-10-11 10:58:18 +0300

    hometask #2 was done

--- Workshop/Main.cs
+++ Workshop/Main.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using Hometask1;
-
+using Hometask2.Exceptions;
 namespace Workshop
 {
     class WorkshopMain
     {
         static void Main(string[] args)
         {
-            Program.Main(args);
-            
-            Console.WriteLine("Hello World!");
+            Hometask1.Hometask1.Main(args);
+            Hometask2.Exceptions.Hometask2.Main(args);
             Console.ReadKey();
 
         }
